cf13fb113dab4d3549e81b87d5912fb83aa80618
oleksii.svarychevskyi <oleksii.svarychevskyi@de.atu.eu>
2017-08-29 13:50:55 +0200

    - added token generation

--- src/login/containers/Login/Login.js
+++ src/login/containers/Login/Login.js
@@ -3,10 +3,14 @@ import { observer } from 'mobx-react';
 import * as routes from '../../../constants/routes';
 import * as storage from '../../../constants/storage';
 
+import Snackbar from 'material-ui/Snackbar';
+
 import LoginForm from '../../components/LoginForm/Form';
 import FieldStore from '../../stores/form/field';
 
 import Form from '../../components/Form/Form';
+
+const Base64 = require('js-base64').Base64;
 
 const stylesPaper = {
     padding: 15,
@@ -14,26 +18,42 @@ const stylesPaper = {
     margin: 5,
 };
 
+function _checkTwoFactor(response) {
+    console.log("check");
+    console.log(response.headers.get('X-GitHub-OTP'));
+    console.log(response.status);
+    const twoFactor = response.headers.get('X-GitHub-OTP');
+    if (twoFactor && twoFactor.indexOf("required") !== -1) {
+        throw { status: response.status, twoFactor: true };
+    }
+    return response;
+}
+
 @observer
-class App extends Component {
+class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
             fields: {
-                userName: new FieldStore({ name: 'user_name', value: '', placeholder: 'Enter git user name' }),
-                token: { name: 'token', value: '', placeholder: 'Enter git token' },
-                password: new FieldStore({ name: 'password', value: '', placeholder: 'password', type: 'password' }),
-                code: { name: 'code', value: '', placeholder: 'Enter received code' },
+                username: new FieldStore({ name: 'username', value: '', placeholder: 'GitHub User Name' }),
+                token: { name: 'token', value: '', placeholder: 'GitHub token' },
+                password: new FieldStore({ name: 'password', value: '', placeholder: 'GitHub Password', type: 'password' }),
+                code: { name: 'code', value: '', placeholder: '2FA Code' },
             },
             showLoginForm: true,
             showGenerateForm: false,
             showCodeForm: false,
+            error: {
+                open: false,
+                message: ''
+            }
         };
 
         this.showForm = this.showForm.bind(this);
         this.onLoginSubmit = this.onLoginSubmit.bind(this);
         this.onTokenGenSubmit = this.onTokenGenSubmit.bind(this);
         this.onCodeSubmit = this.onCodeSubmit.bind(this);
+        this.handleErrorMessageClose = this.handleErrorMessageClose.bind(this);
     }
 
     componentWillMount() {
@@ -55,25 +75,158 @@ class App extends Component {
     }
 
     onLoginSubmit(data) {
-        const auth = {
-            token: data.token,
-            username: data.user_name
-        };
-
-        this.saveAuthToLocalStorage(auth);
-        this.goToHome();
-    }
-
-    saveAuthToLocalStorage(auth) {
-        localStorage.setItem(storage.AUTH, JSON.stringify(auth));
+        fetch('https://api.github.com/user', {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `token ${data.token}`
+            }
+        })
+        .then(response => {
+            if (response.status !== 200) {
+                throw new Error('Token is not valid.');
+            }
+            return response.json()
+        })
+        .then(userData => {
+            this._storeCredentials(userData.login, data.token);
+            this.goToHome();
+        })
+        .catch(() => {
+            this.setState({
+                error: {
+                    open: true,
+                    message: 'Token is not valid'
+                }
+            });
+        });
     }
 
     onTokenGenSubmit(data) {
-        this.showForm('code');
+        this._generateToken(data.username, data.password);
     }
 
     onCodeSubmit(data) {
+        const username = this.state.fields.username.value;
+        const password = this.state.fields.password.value;
+        const twoFactorCode = data.code;
+        this._generateToken(username, password, twoFactorCode);
+    }
 
+    _generateToken(username, password, twoFactorCode) {
+        this
+        ._createToken(username, password, twoFactorCode)
+        .then(_checkTwoFactor)
+        .then(response => {
+            console.log(response.status);
+            console.log(response.json());
+            if (response.status === 422) {
+                return this
+                    ._getTokens(username, password, twoFactorCode)
+                    .then(response => response.json())
+                    .then(tokens => {
+                        const token = this._findToken(tokens, "Radaller CMS");
+                        if (token) {
+                            return this._deleteToken(token, username, password, twoFactorCode)
+                        } else {
+                            return Promise.resolve();
+                        }
+                    })
+                    .then(() => {
+                        return this._createToken(username, password, twoFactorCode);
+                    })
+            } else {
+                return response;
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            this._storeCredentials(username, data.token);
+            this.goToHome();
+        })
+        .catch((e) => {
+            console.log(e);
+            if (e.twoFactor) {
+                this.showForm('code');
+            } else {
+                this.setState({
+                    error: {
+                        open: true,
+                        message: 'Credentials are not valid.'
+                    }
+                });
+            }
+        });
+    }
+
+    _createToken(username, password, twoFactorCode) {
+        let headers = new Headers();
+        headers.append("Content-Type", "application/json");
+        headers.append("Authorization", 'Basic ' + Base64.encode(username + ":" + password));
+        if (twoFactorCode) {
+            headers.append("X-GitHub-OTP", twoFactorCode);
+        }
+        return fetch('https://api.github.com/authorizations', {
+            method: 'POST',
+            mode: 'cors',
+            headers: headers,
+            body: JSON.stringify({
+                "scopes": [
+                    "public_repo"
+                ],
+                "note": "Radaller CMS"
+            })
+        });
+    }
+
+    _getTokens(username, password, twoFactorCode) {
+        let headers = new Headers();
+        headers.append("Content-Type", "application/json");
+        headers.append("Authorization", 'Basic ' + Base64.encode(username + ":" + password));
+        if (twoFactorCode) {
+            headers.append("X-GitHub-OTP", twoFactorCode);
+        }
+        return fetch('https://api.github.com/authorizations', {
+            method: 'GET',
+            mode: 'cors',
+            headers: headers
+        });
+    }
+
+    _findToken(tokens, note) {
+        return tokens.find(token => token.note === note);
+    }
+
+    _deleteToken(token, username, password, twoFactorCode) {
+        let headers = new Headers();
+        headers.append("Content-Type", "application/json");
+        headers.append("Authorization", 'Basic ' + Base64.encode(username + ":" + password));
+        if (twoFactorCode) {
+            headers.append("X-GitHub-OTP", twoFactorCode);
+        }
+        return fetch('https://api.github.com/authorizations/' + token.id, {
+            method: 'DELETE',
+            mode: 'cors',
+            headers: headers
+        })
+    }
+
+    _storeCredentials(login, token) {
+        const auth = {
+            token: token,
+            username: login
+        };
+        localStorage.setItem('auth', JSON.stringify(auth));
+    }
+
+    handleErrorMessageClose() {
+        this.setState({
+            error: {
+                open: false,
+                message: ''
+            }
+        });
     }
 
     render() {
@@ -82,7 +235,7 @@ class App extends Component {
                 {
                     this.state.showLoginForm && (
                         <LoginForm
-                            fields={ [this.state.fields.userName, this.state.fields.token] }
+                            fields={ [this.state.fields.token] }
                             onSubmit={ this.onLoginSubmit }
                             onTokenGeneratePress={ () => {
                                 this.showForm('generate')
@@ -94,7 +247,7 @@ class App extends Component {
                 {
                     this.state.showGenerateForm && (
                         <Form
-                            fields={ [this.state.fields.userName, this.state.fields.password] }
+                            fields={ [this.state.fields.username, this.state.fields.password] }
                             onSubmit={ this.onTokenGenSubmit }
                             onCancelPress={ () => {
                                 this.showForm('login')
@@ -114,9 +267,15 @@ class App extends Component {
                         />
                     )
                 }
+                <Snackbar
+                    open={this.state.error.open}
+                    message={this.state.error.message}
+                    autoHideDuration={4000}
+                    onRequestClose={this.handleErrorMessageClose}
+                />
             </div>
         );
     }
 }
 
-export default App;
+export default Login;
