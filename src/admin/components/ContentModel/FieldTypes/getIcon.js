64af0eaabe6560762a200d230bde380bc0f6d1d9
shved <shved@shved.local>
2017-09-04 13:59:15 +0200

    added field type form window, refactored logic

---
+++ src/admin/components/ContentModel/FieldTypes/getIcon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TextIcon from 'material-ui/svg-icons/editor/format-align-justify';
+
+const style = {
+    fill: '#03A9F4',
+};
+
+export default (type) => {
+    switch(type) {
+        case 'text':
+            return <TextIcon style={ style } />;
+    }
+}
