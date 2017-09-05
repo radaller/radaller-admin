8f24f05b94bb7996113eb2349aa0a046e74a04f7
oleksii.svarychevskyi <oleksii.svarychevskyi@de.atu.eu>
2017-09-05 17:55:53 +0200

    - added timeouts

--- test/spec/open.admin.test.js
+++ test/spec/open.admin.test.js
@@ -10,6 +10,7 @@ describe('Admin', function() {
             .goto(appUrl)
             .insert('input[name="token"]', 'valid_token')
             .click('.next-button button')
+            .wait(500)
             .wait('.add-button button')
             .click('.add-button button')
             .insert('input[name="repo_name"]', 'test/test-repository-1')
@@ -21,15 +22,17 @@ describe('Admin', function() {
         it('should open/close admin', async function () {
             let repoName = await nightmare
                 .click('.repo-item')
+                .wait(1000)
                 .wait('.logout')
                 .click('.logout')
+                .wait(500)
                 .wait('.add-button button')
                 .evaluate(() => {
                     return document.querySelector('.repo-item span').innerText
                 })
                 .end();
             expect(repoName).toEqual('test/test-repository-1');
-        }, 20000);
+        }, 30000);
     })
 
 });
