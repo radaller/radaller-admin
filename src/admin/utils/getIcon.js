d5636e8439c5a37325b39086abec91fb2eed295f
shved <shved@shved.local>
2017-09-06 11:31:00 +0200

    Added property visual representation

--- src/admin/utils/getIcon.js
+++ src/admin/utils/getIcon.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import TextIcon from 'material-ui/svg-icons/editor/format-align-justify';
+import TextIcon from 'material-ui/svg-icons/content/text-format';
+import DefaultIcon from 'material-ui/svg-icons/content/add';
 
 const style = {
     fill: '#03A9F4',
@@ -9,5 +10,8 @@ export default (type) => {
     switch(type) {
         case 'TextInput':
             return <TextIcon style={ style } />;
+
+        default:
+            return <DefaultIcon style={ style } />;
     }
 }
